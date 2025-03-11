be12be353c7355c6167adb07516bceacb20d640d
kazantsevnik <kazantsevnik12@gmail.com>
2025-03-11 13:14:52 +0500

    Dependency Injections

--- src/main/java/com/example/start/StartApplication.java
+++ src/main/java/com/example/start/StartApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StartApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
     }
 
+    public StartApplication (Plane plane) {
+        plane.takeOff();
+    }
 }
